26d50a8cff28ae2fc7b305b9498d4febeac23d5a
celmerr89 <maciej.celmer89@gmail.com>
2019-02-26 18:26:55 +0100

    reading data

--- HashCode2019/HashCode2019/main.cpp
+++ HashCode2019/HashCode2019/main.cpp
@@ -1,6 +1,43 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
+
+int R, C, L, H;
+vector<vector<char> > Pizza;
+
+
+void ReadData() {
+
+    cin >> R >> C >> L >> H;
+
+    Pizza.resize(R);
+    for (int i = 0; i < R; ++i) {
+        Pizza[i].resize(C);
+    }
+
+    for (int i = 0; i < R; ++i) {
+        for (int j = 0; j < C; ++j) {
+            cin >> Pizza[i][j];
+        }
+
+    }
+}
+
+
+void Solve()
+{
+
+    // TO DO
+}
+
 
 int main()
 {
+    ReadData();
+    Solve();
+
     return 0;
 }
