23c6dac3eae62399fa707cd48c2f629c9b8b9b9b
MAntkiewicz <antkiewicz.michal@gmail.com>
2020-02-20 20:24:13 +0100

    Hyper optimization for C

--- HashCode2020/HashCode2020/ScoreC.h
+++ HashCode2020/HashCode2020/ScoreC.h
@@ -4,5 +4,15 @@
 
 double CalculateLibraryScore(int library_idx, int days)
 {
-    return LIBRARIES[library_idx].books.size();
+	float score = 0;
+	auto& l = LIBRARIES[library_idx];
+
+	int book_idx = 0;
+	for (int d = 0; d < (days - l.T) * l.M && book_idx < l.books.size(); ++d)
+	{
+		score += BOOKS[l.books[book_idx]];
+		book_idx++;
+	}
+
+	return score / LIBRARIES[library_idx].T;
 }
