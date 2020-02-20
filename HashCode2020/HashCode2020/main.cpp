c59b1888904dff5b3b33e9fcd68ce7db436b3a1e
celmerr89 <maciej.celmer89@gmail.com>
2020-02-20 19:17:15 +0100

    data parse

--- HashCode2020/HashCode2020/main.cpp
+++ HashCode2020/HashCode2020/main.cpp
@@ -15,9 +15,59 @@
 
 using namespace std;
 
+
+unsigned long long B, L, D;
+
+vector<unsigned long long> BOOKS;
+
+struct Library
+{
+	unsigned long long N; //number of books in library
+	unsigned long long T; // singup process time
+	unsigned long long M; // books per day
+	vector<unsigned long long> books;
+};
+
+vector<Library> LIBRARIES;
+
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	cin >> B >> L >> D;
+
+	// BOOKS
+	BOOKS.reserve(B);
+
+	for (unsigned long long i = 0; i < B; ++i)
+	{
+		unsigned long long book;
+		cin >> book;
+		BOOKS.push_back(book);
+	}
+
+
+	//LIBRARIES
+	LIBRARIES.reserve(L);
+
+	for (unsigned long long i = 0; i < L; ++i)
+	{
+		Library library; 
+		cin >> library.N >> library.T >> library.M;
+
+
+		library.books.reserve(library.N);
+
+
+		for (unsigned long long j = 0; j < library.N; ++j)
+		{
+			unsigned long long book;
+			cin >> book;
+			library.books.push_back(book);
+		}
+
+
+		LIBRARIES.push_back(std::move(library));
+	}
+
 
 
 	return 0;
