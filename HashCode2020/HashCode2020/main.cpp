240777b1091c1d3bd972b418708b07474a9f7892
celmerr89 <maciej.celmer89@gmail.com>
2020-02-20 18:10:05 +0100

    adding very important includes

--- HashCode2020/HashCode2020/main.cpp
+++ HashCode2020/HashCode2020/main.cpp
@@ -1,6 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<list>
+#include<cassert>
+#include <algorithm>
+#include <memory>
+#include <tuple>
+#include <cmath>
+#include <map>
+#include <set>
+#include <unordered_map>
 #include <iostream>
+#include <string>
+#include <math.h>    
+
+using namespace std;
 
 int main()
 {
     std::cout << "Hello World!\n"; 
+
+
+	return 0;
 }
