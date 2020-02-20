498e7088ddc41a98de6888b2ae55cf6a77d57385
Adam Wilk <adam.wilk@outlook.com>
2020-02-20 19:40:44 +0100

    calculate score

--- HashCode2020/HashCode2020/main.cpp
+++ HashCode2020/HashCode2020/main.cpp
@@ -26,9 +26,18 @@ struct Library
 	unsigned long long T; // singup process time
 	unsigned long long M; // books per day
 	vector<unsigned long long> books;
+
+    bool is_commited = false;
+    int score = 0;
 };
 
 vector<Library> LIBRARIES;
+
+
+int CalculateLibraryScore(int library_idx, int days)
+{
+    return 0;
+}
 
 int main()
 {
@@ -68,6 +77,42 @@ int main()
 		LIBRARIES.push_back(std::move(library));
 	}
 
+
+    vector<int> remaining_libraries;
+    int days_left = D;
+    unsigned long long total_score = 0;
+
+    for (int commited_libraries = 0; commited_libraries < LIBRARIES.size(); ++commited_libraries)
+    {
+        // Sort books in each library.
+        for (int lib = 0; lib < LIBRARIES.size(); ++lib)
+        {
+            if (!LIBRARIES[lib].is_commited)
+            {
+                auto& books = LIBRARIES[lib].books;
+                std::sort(books.begin(), books.end(), [&](auto lhs, auto rhs) { return BOOKS[lhs] > BOOKS[rhs]; });
+            }
+        }
+
+        // Calculate score for each library.
+        remaining_libraries.clear();
+        remaining_libraries.reserve(LIBRARIES.size() - commited_libraries);
+
+        for (int lib = 0; lib < LIBRARIES.size(); ++lib)
+        {
+            if (!LIBRARIES[lib].is_commited)
+            {
+                LIBRARIES[lib].score = CalculateLibraryScore(lib, days_left);
+                remaining_libraries.emplace_back(lib);
+            }
+        }
+
+        // Choose library with the highest score.
+        int chosen_one = *std::max_element(remaining_libraries.begin(), remaining_libraries.end(), [&](int lhs, int rhs) { return LIBRARIES[lhs].score < LIBRARIES[rhs].score; });
+
+
+
+    }
 
 
 	return 0;
