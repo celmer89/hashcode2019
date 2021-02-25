7fc425edf530e7901cfb08b7f76bd68537f3bc9c
Jacek Szymonek <jacksimons256@gmail.com>
2021-02-25 21:05:02 +0100

    process all script

---
+++ HashCode2021/process_all.bat
@@ -0,0 +1,8 @@
+@echo off
+
+x64\Release\HashCode2020.exe < HashCode2020\input_a.txt > HashCode2020\output_a.txt
+x64\Release\HashCode2020.exe < HashCode2020\input_b.txt > HashCode2020\output_b.txt
+x64\Release\HashCode2020.exe < HashCode2020\input_c.txt > HashCode2020\output_c.txt
+x64\Release\HashCode2020.exe < HashCode2020\input_d.txt > HashCode2020\output_d.txt
+x64\Release\HashCode2020.exe < HashCode2020\input_e.txt > HashCode2020\output_e.txt
+x64\Release\HashCode2020.exe < HashCode2020\input_f.txt > HashCode2020\output_f.txt
