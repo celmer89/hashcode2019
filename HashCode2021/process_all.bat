454d35ef87dee1eff71fb7054b76898b0d3a9fae
celmerr89 <maciej.celmer89@gmail.com>
2021-02-25 21:25:01 +0100

    process all log

--- HashCode2021/process_all.bat
+++ HashCode2021/process_all.bat
@@ -1,8 +1,13 @@
-@echo off
-
+echo start
 x64\Release\HashCode2020.exe < HashCode2020\input_a.txt > HashCode2020\output_a.txt
+echo A processed
 x64\Release\HashCode2020.exe < HashCode2020\input_b.txt > HashCode2020\output_b.txt
+echo B processed
 x64\Release\HashCode2020.exe < HashCode2020\input_c.txt > HashCode2020\output_c.txt
+echo C processed
 x64\Release\HashCode2020.exe < HashCode2020\input_d.txt > HashCode2020\output_d.txt
+echo D processed
 x64\Release\HashCode2020.exe < HashCode2020\input_e.txt > HashCode2020\output_e.txt
+echo E processed
 x64\Release\HashCode2020.exe < HashCode2020\input_f.txt > HashCode2020\output_f.txt
+echo F processed
