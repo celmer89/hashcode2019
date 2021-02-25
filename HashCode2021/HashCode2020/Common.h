af53dfca98fae14578eb21a2113ab9b20262f788
celmerr89 <maciej.celmer89@gmail.com>
2021-02-25 18:29:48 +0100

    hc 2021 kickoff

---
+++ HashCode2021/HashCode2020/Common.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include<iostream>
+#include<vector>
+#include<list>
+#include<cassert>
+#include <algorithm>
+#include <memory>
+#include <tuple>
+#include <cmath>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <iostream>
+#include <string>
+#include <math.h>    
+
+using namespace std;
+
+
---
+++ HashCode2021/HashCode2020/main.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+#include<list>
+#include<cassert>
+#include <algorithm>
+#include <memory>
+#include <tuple>
+#include <cmath>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <iostream>
+#include <string>
+#include <math.h>    
+
+using namespace std;
+
+#include "Common.h"
+
+int main()
+{
+	
+
+	return 0;
+}
