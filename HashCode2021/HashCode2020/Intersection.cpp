14ccb9947dbf8c8616f9e03755b2d6a43e4022c5
celmerr89 <maciej.celmer89@gmail.com>
2021-02-25 21:13:21 +0100

    scheduling

--- HashCode2021/HashCode2020/Intersection.cpp
+++ HashCode2021/HashCode2020/Intersection.cpp
@@ -4,7 +4,26 @@
 
 void Intersection::Schedule()
 {
-	schedule.schedule.push_back(make_pair(1, 2));
+	vector<Street*> streets;
 
-	// TODO
+	for (auto& str : in_streets)
+	{
+		if (str.second.num_cars_global > 0)
+		{
+			streets.push_back(&str.second);
+		}
+	}
+
+	sort(streets.begin(), streets.end(), [](const auto& a, const auto& b)
+	{
+		return a->num_cars_global > b->num_cars_global;
+	});
+
+	int min_num_cars = streets.back()->num_cars_global;
+
+	for (auto& str : streets)
+	{
+		int timebox = str->num_cars_global / min_num_cars + 0.5f;
+		schedule.schedule.push_back(make_pair(str->id, timebox));
+	}
 }
